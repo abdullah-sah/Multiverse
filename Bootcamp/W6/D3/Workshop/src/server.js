43db5124d93e38223476f6732376e7d70c5c4eef
Abdullah Sahraoui <abdullahsahraoui@Abdullahs-MacBook-Air.local>
2022-11-10 15:55:25 +0000

    Added files for week 6 day 4

--- Bootcamp/W6/D3/Workshop/src/server.js
+++ Bootcamp/W6/D3/Workshop/src/server.js
@@ -10,11 +10,46 @@ const app = express();
 // import userRouter for use in web server
 const userRouter = require("./routes/user");
 
+// middleware. ALWAYS PLACE IN MIDDLEWARE FOLDER!!
+function middle(req, res, next) {
+	try {
+		if (req.body.name) {
+			req.body.name = req.body.name.toUpperCase();
+		}
+		next();
+	} catch (error) {
+		// two ways of handling the error:
+		// next() allows error handling in Express app, res.send(error) affords error handling in the client
+		// next(error); // pass the error along the chain of functions/controllers on the endpoint
+		res.status(404).send(error); // Stop progress on the endpoint and send an error from this location
+	}
+}
+
+function passLength(req, res, next) {
+	try {
+		if (req.body.password && req.body.password.length < 8) {
+			throw new Error("Password not long enough");
+		}
+		next();
+	} catch (error) {
+		res.status(500).send(error);
+	}
+}
+
 // allow web server to pass json
-app.use(express.json());
+app.use(express.json()); //express.json() returns a function, that's why you need the parentheses
+
+// adding middleware to all endpoints
+// app.use(middle);
 
 // .use is self explanatory - tell's 2nd param to use the first param
-app.use("/user", userRouter);
+app.use("/user", passLength, userRouter);
+
+// endpoint to sync db
+app.get("/sync", async (req, res) => {
+	await db.sync({ force: true });
+	res.sendStatus(200);
+});
 
 // listen on port
 app.listen(5001, async () => {
